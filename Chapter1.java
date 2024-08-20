b62d29801d77203695c7d7b488091a62a63156ff
Matt Callaghan <mcallaghan@naperville203.org>
2024-08-20 14:20:28 -0500

    Started chapter 1 notes

---
+++ Chapter1.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class Chapter1 here.
+ *
+ * @author mrcallaghan
+ * @version 20aug2024
+ * 
+ * Every Java program contains one or more classes (e.g., Chapter1).
+ * 
+ * In general, every source file (e.g., Chapter1.java) conatins
+ *  one class (e.g., Chapter1).
+ *  
+ *  The source file name must match the class name.
+ *  
+ *  By convention, class names start with an uppercase letter.
+ */
+
+import java.awt.Color;
+
+public class Chapter1
+{
+    
+    /*
+     * A class contains method.
+     * 
+     * Most Java programs contain a class with a main method.
+     * 
+     * The main method is executed when the program starts.
+     */
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello, world.");
+    }
+
+}
