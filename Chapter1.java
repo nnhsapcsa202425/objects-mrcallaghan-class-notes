ee8680e39767abe72ff069e4b84e3109e3ffc52a
Matt Callaghan <mcallaghan@naperville203.org>
2024-08-20 15:07:03 -0500

    Wrote Hello World in Java

---
+++ Chapter1.java
@@ -0,0 +1,42 @@
+
+/**
+ * Chapter 1 Notes.
+ *
+ * @author mrcallaghan
+ * @version 20aug2024
+ * 
+ * Every Java program contains one or more classes (e.g., Chapter1).
+ * 
+ * In general, every source file (e.g., Chapter1.java) contains one class (e.g., Chapter1).
+ * 
+ * The source file name must match the class name.
+ * 
+ * By convention, class names start with an uppercase letter.
+ * 
+ */
+
+import java.awt.Color;
+
+public class Chapter1
+{
+    
+    /*
+     * A class contains methods.
+     * 
+     * Most Java programs contain a class with a main method.
+     * 
+     * The main method is executed when the program starts.
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, world.");
+    }
+}
+
+
+
+
+
+
+
+
