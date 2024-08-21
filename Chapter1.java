ec7c23877c4daa213f28bfa13a79cc9db1db7783
Matt Callaghan <mcallaghan@naperville203.org>
2024-08-21 13:21:05 -0500

    Continued CH 1 notes

--- Chapter1.java
+++ Chapter1.java
@@ -1,14 +1,25 @@
-
 /**
- * Write a description of class Chapter1 here.
+ * Chapter 1 Notes Project
  *
  * @author mrcallaghan
  * @version 20aug2024
  */
 /*
- * In general, every source file, Ie.g., Chapter1
+ * In general, every source file, (e.g., Chapter1.java) contains one or more classes (e.g., Chapter1).
+ * 
+ * Every Java program contains one or more classes (e.g., Chapter1).
+ * 
+ * The source file name and the class name must match.
+ * 
+ * By convention, class names must start with an uppercase.
  */
+
+import java.awt.Color;
+
 public class Chapter1
 {
-
+    public static void main(String[] args)
+    {
+        
+    }
 }
