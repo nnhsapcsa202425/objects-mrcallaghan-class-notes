48c9478556f7202dc821a0aae77623be58d18ab1
Matt Callaghan <mcallaghan@naperville203.org>
2024-08-22 15:10:07 -0500

    Almost done with errors notes

--- Chapter1.java
+++ Chapter1.java
@@ -30,6 +30,81 @@ public class Chapter1
     public static void main(String[] args)
     {
         System.out.println("Hello, world again.");
+        World ocean = new World();
+        Turtle crush = new Turtle(ocean);
+        crush.setPenColor(Color.ORANGE);
+        crush.penDown();
+        crush.forward(50);
+        /*
+         * A method contains statements.
+         * 
+         * Statments may invoke other methods (e.g., forward).
+         * 
+         * In general, methods are invoked on objects (e.g., crush).
+         * 
+         * When invoking a method, arguments are passed in parentheses. (e.g., 50)
+         */
+        
+        crush.forward(60);
+        
+        /*
+         * A statement contains identifiers.
+         * 
+         * Identifiers may have any combo of:
+         *      letters
+         *      numbers (but not for the first character)
+         *      underscores (_)
+         *      dollar signs ($)
+         *      
+         * These are valid identifiers:
+         */
+        int y, x2, X_Y, total$;
+        
+        // this is not a valid identifier
+        //int 2x;
+        
+        /*
+         * Identifiers are case sensitive (case matters)
+         * 
+         * These are different:
+         */
+        Turtle squirt, SQUIRT, sQuIRt;
+        
+        // variables cannot be redefined with the same name in the same method
+        //Turtle squirt;
+        squirt = new Turtle(ocean);
+        
+        /*
+         * By convention, constants are all uppercase.
+         *      Use an underscore to separate words.
+         */
+        int SPEED_OF_LIGHT = 30000000; //m/s
+        
+        /*
+         * By convention, most other identifiers start with a lowercase letter.
+         *  Subsequent words start with an uppercase letter (CamalCase).
+         */
+        World pacificOcean;
+        
+        /*
+         * This is a compile-time error. (This code will not compile.)
+         * 
+         * These are often called syntax errors.
+         */
+        //crush.forward(40)
+        //crush.forward(40;
+        //crush.forwrd(40);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
     }
 }
